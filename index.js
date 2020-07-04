4536bce39334862557ec1974e27deb2c068c83bf
Zainab Azzam <zainab_azzam@yahoo.com>
2020-07-04 10:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-// Add your code here
+// // Add your code here
+// let formData = {
+//   dogName: 'Boby',
+//   dogBreed: 'sadfgs',
+// };
+
+// let configObj = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json',
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// function render(object) {
+//   document.body.insertAdjacentHTML('beforebegin', `<p>${object.dogName}</p>`);
+// }
+
+// document.querySelector('button').addEventListener('click', () => {
+//   fetch('http://localhost:3000/dogs', configObj)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       render(object);
+//     })
+//     .catch(function (error) {
+//       alert('Bad things! Ragnarők!');
+//       console.log(error.message);
+//     });
+// });
+
 function submitData(name, email) {
-    let formData = {
-        name: name,
-        email: email
-    };
+  const newUser = {
+    name,
+    email,
+  };
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML = data.id;
+      console.log(data.id);
+    })
+    .catch(error => {
+      alert('stop stop stop');
+      document.body.innerHTML = error.message;
+      console.log(error.message);
+    });
+}
 
-    let configObj = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(formData)
-    };
-
-    return fetch("http://localhost:3000/users", configObj)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(object) {
-            let h2 = document.createElement('h2');
-            h2.innerHTML = object.id;
-            document.body.appendChild(h2);
-            console.log(object);
-        })
-        .catch(function(error) {
-            let h3 = document.createElement('h3');
-            h3.innerHTML = error.message;
-            document.body.appendChild(h3);
-            console.log(error.message);
-        });}
+submitData('test', 'test.@test.com');
